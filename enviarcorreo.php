0a18b0df3964ff454295d259d54908fca00a0d93
Juan <jerosario@itla.edu.do>
2023-07-02 08:40:44 -0400

    initial

---
+++ enviarcorreo.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $nombre = $_POST['nombre'];
+  $correo = $_POST['correo'];
+  // Obtén otros datos del formulario
+
+  // Configura el correo
+  $destinatario = 'tucorreo@example.com'; // Cambia esto con tu dirección de correo
+  $asunto = 'Nuevo formulario de contacto';
+  $mensaje = "Nombre: $nombre\n";
+  $mensaje .= "Correo electrónico: $correo\n";
+  // Agrega otros campos al mensaje
+
+  // Envía el correo
+  $resultado = mail($destinatario, $asunto, $mensaje);
+
+  if ($resultado) {
+    echo 'El correo ha sido enviado correctamente.';
+  } else {
+    echo 'Ocurrió un error al enviar el correo.';
+  }
+}
+?>
